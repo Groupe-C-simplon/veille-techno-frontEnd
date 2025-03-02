845a2ecc0808985d2d9020ea9084d643341f2253
Groupe-C-simplon <groupe.c.devweb2025p6@gmail.com>
2025-03-02 17:15:44 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// Compteur
+let count = 0;
+
+function increment() {
+    count++;
+    document.getElementById('count').textContent = count;
+}
+
+function decrement() {
+    count--;
+    document.getElementById('count').textContent = count;
+}
